dafa86fde91c1461aa6f54e243d61450059b9748
Franco Panozzo <francopanozzo@hotmail.com.ar>
2021-10-31 02:42:23 +0100

    Save new authenticated users to our DB

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,30 @@ export default {
     components: {
         TheNavbar,
     },
+    mounted() {
+        this.saveUser()
+    },
+    methods: {
+        async saveUser() {
+            try {
+                const user = this.$auth.user
+
+                // When logging in try to save the user (won't save if it's not new)
+                const res = await this.$api.post('users', {
+                    ...user,
+                    first_name: user.given_name || '',
+                    last_name: user.family_name || '',
+                    username: user.nickname || '',
+                    profile_pic: user.picture || null,
+                    is_email_verified: user.email_verified,
+                    DOB: new Date(),
+                    ui_theme: this.$store.state.UI.theme || 'light',
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        },
+    },
 }
 </script>
 
